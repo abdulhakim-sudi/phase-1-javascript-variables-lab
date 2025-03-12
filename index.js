bd2c78d944c90b20c1380e5a8ee0d66ff7f3c459
ABDULHAKIM <abdulhakimsudi27@gmail.com>
2025-03-12 10:14:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
+const companyName =  "scuber"; // defined as a const
+let mostProfitableNeighborhood = "chelse"; // defined using let
+let companyCeo = "susan smith"; // defined using let
